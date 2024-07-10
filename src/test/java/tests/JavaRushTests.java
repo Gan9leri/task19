896769193747009546410141a2a46541f0db3e48
AOvsyannikov <aovsyannikov@it-one.ru>
2024-07-10 22:37:15 +0300

    updated WebConfig.java

--- src/test/java/tests/JavaRushTests.java
+++ src/test/java/tests/JavaRushTests.java
@@ -1,10 +1,16 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.QuestsPage;
 
+@DisplayName("UI тесты")
+@Tag("ui")
 public class JavaRushTests extends TestBase{
 
+    @DisplayName("Тест открытия курсов")
+    @Tag("GoToQuestsTest")
     @Test
     void GoToQuestsTest(){
         QuestsPage step = new QuestsPage();
